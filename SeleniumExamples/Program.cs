ac87d5a9d3f8d4bfa6d723ef8d00e9f4d89c81aa
deepaksivaraman7 <deepak.sivaraman@ust.com>
2023-11-10 15:09:11 +0530

    Add project files.

---
+++ SeleniumExamples/Program.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver=new ChromeDriver();
+driver.Url = "https://www.google.com/";
+Thread.Sleep(5000);
+string title = "Google";
+try
+{
+    Assert.AreEqual("Google", title);
+    Console.WriteLine("Test passed");
+}
+catch (AssertionException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+driver.Close();
