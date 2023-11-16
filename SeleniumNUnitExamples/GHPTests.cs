53f4b9b7cfcec7c040037705e23c3c586221ef78
deepaksivaraman7 <deepak.sivaraman@ust.com>
2023-11-16 19:04:29 +0530

    16-11-2023

---
+++ SeleniumNUnitExamples/GHPTests.cs
@@ -0,0 +1,60 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumNUnitExamples
+{
+    [TestFixture]
+    internal class GHPTests:CoreCodes
+    {
+        [Test]
+        [Order(1)]
+        public void TitleTest()
+        {
+            Thread.Sleep(5000);
+            string title = driver.Title;
+            Assert.AreEqual("Google", title);
+        }
+        [Test]
+        [Order(2)]
+        public void GSTest()
+        {
+            IWebElement searchInputTextBox = driver.FindElement(By.Id("APjFqb"));
+            searchInputTextBox.SendKeys("hp laptop");
+            Thread.Sleep(2000);
+            IWebElement googleSearchButton = driver.FindElement(By.ClassName("gNO89b"));//Name("btnK"));
+            googleSearchButton.Click();
+            Assert.AreEqual("hp laptop - Google Search", driver.Title);
+        }
+        [Test]
+        public void ALlLinksStatusTest()
+        {
+            List<IWebElement> allLinks = driver.FindElements(By.TagName("a")).ToList();
+            foreach(var link in allLinks)
+            {
+                string url=link.GetAttribute("href");
+                if (url == null)
+                {
+                    Console.WriteLine("Url is null");
+                    continue;
+                }
+                else
+                {
+                    bool isWorking = CheckLinkStatus(url);
+                    if (isWorking)
+                    {
+                        Console.WriteLine(url + " is working");
+                    }
+                    else
+                    {
+                        Console.WriteLine(url + " is not working");
+                    }
+                }
+            }
+        }
+    }
+}
