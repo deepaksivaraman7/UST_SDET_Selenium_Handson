5e577ef7496469e40dc2976851224fff3c3a74d7
deepaksivaraman7 <deepak.sivaraman@ust.com>
2023-11-25 09:58:25 +0530

    24-11-2023

--- Naaptol/TestScripts/ProductPurchaseTests.cs
+++ Naaptol/TestScripts/ProductPurchaseTests.cs
@@ -19,40 +19,53 @@ namespace Naaptol.TestScripts
         {
             
             var homePage = new NaaptolHomePage(driver);
+
             if (!driver.Url.Equals("https://www.naaptol.com/"))
             {
                 driver.Navigate().GoToUrl("https://www.naaptol.com");
             }
+
             string? currDir = Directory.GetParent(@"../../../")?.FullName;
             string? excelFilePath = currDir + "/TestData/InputData.xlsx";
             string? sheetName = "ProductSearch";
             List<SearchProduct> excelDataList = ExcelUtils.ReadExcelData(excelFilePath, sheetName);
+
             foreach (var excelData in excelDataList)
             {
                 try
                 {
                     string? searchText = excelData.SearchText;
                     string? productposition = excelData.ProductPosition;
+
                     homePage.ClearSearchBar();
                     homePage.SearchProductType(searchText);
 
                     var productsPage = homePage.SearchButtonClick();
                     productsPage.ProductSelect(productposition);
+
                     List<string> listWindows = driver.WindowHandles.ToList();
                     driver.SwitchTo().Window(listWindows[1]);
+
                     var productPage = new ProductPage(driver);
                     productPage.SelectSize();
                     productPage.BuyButtonClick();
+
                     TakeScreenshot();
                     Assert.That(productPage.GetCartItemUrl(), Is.EqualTo(productPage.GetCurrentUrl()));
+
                     int prevquantity = productPage.GetQuantity();
                     productPage.QuantityIncrease();
+
                     TakeScreenshot();
                     Assert.That(prevquantity + 1 == productPage.GetQuantity());
+
                     productPage.RemoveProductLinkClick();
+
                     TakeScreenshot();
                     Assert.That(productPage.GetCartItemUrl(), Is.Null);
+
                     productPage.CloseButtonClick();
+
                     test = extent.CreateTest("Add to cart test - Passed");
                     test.Pass("Add to cart Success");
                 }
